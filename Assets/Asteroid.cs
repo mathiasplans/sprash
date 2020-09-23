fa4962903e0505ac77c7ab5415a5b39c9e5361c1
Mathias Plans <mathias.plans@estcube.eu>
2020-09-23 13:43:31 +0300

    Add scripts for movement

---
+++ Assets/Asteroid.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour {
+    [SerializeField] public Position position;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Position = position.coordinates;
+    }
+}
---
+++ Assets/Position.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Position : MonoBehaviour {
+    public Vector2 coordinates;
+    public float power = 1.0f;
+
+    // Start is called before the first frame update
+    void Start() {
+        coordinates = new Vector2(0.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("w")) {
+            this.coordinates.y += power * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown("a")) {
+            this.coordinates.x -= power * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown("s")) {
+            this.coordinates.y -= power * Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown("d")) {
+            this.coordinates.x += power * Time.deltaTime;
+        }
+    }
+}
